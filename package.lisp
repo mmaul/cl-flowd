0a95e8a37c2fcf88c5407fd9ea0406e757d379a0
Mike Maul <mike.maul@gmail.com>
2014-03-10 22:00:17 -0400

    Cleaned export section

--- package.lisp
+++ package.lisp
@@ -6,19 +6,4 @@
         #:cl-annot
         #:cl-annot.class)
   (:shadow #:tag #:stream)
-  (:export 
-   #:+store-magic+ #:+store-version+ #:store-field-tag #:store-field-recv-time
-   #:store-field-proto-flags-tos #:store-field-agent-addr4
-   #:store-field-agent-addr6 #:store-field-src-addr4 #:store-field-src-addr6
-   #:store-field-dst-addr4 #:store-field-dst-addr6 #:store-field-gateway-addr4
-   #:store-field-gateway-addr6 #:store-field-srcdst-port #:store-field-packets
-   #:store-field-octets #:store-field-if-indices #:store-field-agent-info
-   #:store-field-flow-times #:store-field-as-info
-   #:store-field-flow-engine-info #:store-field-crc32 #:store-field-all
-   #:open-log #:read-flow #:close-log #:fields #:tag #:recv-time
-   #:proto-flags-tos #:agent-addr #:src-addr #:dst-addr #:gateway-addr
-   #:src-port #:dst-port #:packets #:if-index-in #:if-index-out
-   #:sys-uptime-ms #:time-sec #:time-nanosec #:netflow-version
-   #:flow-start #:flow-finish #:src-as #:dst-as #:src-mask #:dst-mask
-   #:engine-type #:engine-id #:flow-sequence #:src-net #:dst-net #:octets
-   #:packets #:start-time #:format-ipv4))
+  )
